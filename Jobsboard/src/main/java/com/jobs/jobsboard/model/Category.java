8bddfb84b50e2fd5ac7b839019f9ba7c2268dfb0
sophiesucode <sophzsul@gmail.com>
2022-02-25 14:58:40 -0500

    added constructors

--- Jobsboard/src/main/java/com/jobs/jobsboard/model/Category.java
+++ Jobsboard/src/main/java/com/jobs/jobsboard/model/Category.java
@@ -5,4 +5,18 @@ public class Category {
 private Long id;
 
 private String name;
+
+
+
+public Category(){
+
+}
+
+
+    public Category(Long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    
 }
