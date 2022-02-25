bbdb008336fcd18f79022b4e84a95c41a0846ab8
sophiesucode <sophzsul@gmail.com>
2022-02-25 15:00:16 -0500

    added to string method ,getters, setters

--- Jobsboard/src/main/java/com/jobs/jobsboard/model/Category.java
+++ Jobsboard/src/main/java/com/jobs/jobsboard/model/Category.java
@@ -18,5 +18,27 @@ public Category(){
         this.name = name;
     }
 
-    
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Category{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
+    }
 }
