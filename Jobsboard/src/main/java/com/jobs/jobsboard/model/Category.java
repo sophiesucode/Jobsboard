c2c2bd6df2e92dc92a03be2743cb045e0c9f4f16
sophiesucode <sophzsul@gmail.com>
2022-02-25 14:14:20 -0500

    deleted unnecessary comments, added fields for tables

--- Jobsboard/src/main/java/com/jobs/jobsboard/model/Category.java
+++ Jobsboard/src/main/java/com/jobs/jobsboard/model/Category.java
@@ -1,4 +1,8 @@
 package com.jobs.jobsboard.model;
 
 public class Category {
+
+private Long id;
+
+private String name;
 }
--- Jobsboard/src/main/java/com/jobs/jobsboard/model/Jobs.java
+++ Jobsboard/src/main/java/com/jobs/jobsboard/model/Jobs.java
@@ -1,4 +1,17 @@
 package com.jobs.jobsboard.model;
 
 public class Jobs {
+    private Long id;
+
+    private String job_title;
+
+    private String experience_level;
+
+    private String job_description;
+
+    private String location;
+
+    private int date_posted;
+
+
 }
